682b4802accc1294446ae67a34ff3850bf730426
Ibrahim Uzun <iuzun@marun.edu.tr>
2022-10-08 03:28:03 +0300

    feat: minimal concept

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    //   TODO Add Scope Enum Here
-    // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
     'type-enum': [
       2,
       'always',
